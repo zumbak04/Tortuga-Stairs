3b057e3cda3b77bd0dd5fe885f4d1c9bca22ceed
zumbak04 <46384992+zumbak04@users.noreply.github.com>
2022-03-10 00:18:49 +0400

    Счет
    Вывел счет на экран.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -13,6 +14,8 @@ public class GameManager : MonoBehaviour
 
     private float playerHeight = -0.2f;
     private Vector3 stairsOffset;
+
+    public Text scoreText;
 
     private void Start()
     {
@@ -47,6 +50,7 @@ public class GameManager : MonoBehaviour
     public void IncreaseJumps()
     {
         jumps++;
+        scoreText.text = $"—чет: {jumps}";
         if(jumps % 15 == 0)
         {
             SpawnStairs(player.transform.position + stairsOffset);
